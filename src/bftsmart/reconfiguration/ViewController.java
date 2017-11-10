28a11b490d98c7f8159e2814e14a12b20cb6fd9c
Paulo Rodolfo S. L. Coelho <paulo.coelho@usi.ch>
2017-11-10 16:35:24 +0100

    FIX: store 'currentView' file in 'configHome' folder

--- src/bftsmart/reconfiguration/ViewController.java
+++ src/bftsmart/reconfiguration/ViewController.java
@@ -1,28 +1,27 @@
 /**
-Copyright (c) 2007-2013 Alysson Bessani, Eduardo Alchieri, Paulo Sousa, and the authors indicated in the @author tags
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright (c) 2007-2013 Alysson Bessani, Eduardo Alchieri, Paulo Sousa, and the authors indicated in the @author tags
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package bftsmart.reconfiguration;
 
-import java.net.SocketAddress;
-
-import bftsmart.reconfiguration.util.Configuration;
-import bftsmart.reconfiguration.util.HostsConfig;
 import bftsmart.reconfiguration.util.TOMConfiguration;
 import bftsmart.reconfiguration.views.DefaultViewStorage;
 import bftsmart.reconfiguration.views.View;
 import bftsmart.reconfiguration.views.ViewStorage;
+
+import java.lang.reflect.Constructor;
+import java.net.SocketAddress;
 
 /**
  *
@@ -40,41 +39,45 @@ public class ViewController {
         this.staticConf = new TOMConfiguration(procId);
     }
 
-    
+
     public ViewController(int procId, String configHome) {
         this.configHome = configHome;
         this.staticConf = new TOMConfiguration(procId, configHome);
     }
 
-    
+
     public final ViewStorage getViewStore() {
         if (this.viewStore == null) {
             String className = staticConf.getViewStoreClass();
             try {
-                this.viewStore = (ViewStorage) Class.forName(className).newInstance();
+                if (configHome.equals(""))
+                    this.viewStore = (ViewStorage) Class.forName(className).newInstance();
+                else {
+                    Constructor c = Class.forName(className).getConstructor(String.class);
+                    this.viewStore = (ViewStorage) c.newInstance(configHome);
+                }
             } catch (Exception e) {
                 this.viewStore = new DefaultViewStorage(configHome);
             }
-
         }
         return this.viewStore;
     }
 
-    public View getCurrentView(){
-        if(this.currentView == null){
-             this.currentView = getViewStore().readView();
+    public View getCurrentView() {
+        if (this.currentView == null) {
+            this.currentView = getViewStore().readView();
         }
         return this.currentView;
     }
-    
-    public View getLastView(){
+
+    public View getLastView() {
         return this.lastView;
     }
-    
+
     public SocketAddress getRemoteAddress(int id) {
         return getCurrentView().getAddress(id);
     }
-    
+
     public void reconfigureTo(View newView) {
         this.lastView = this.currentView;
         this.currentView = newView;
